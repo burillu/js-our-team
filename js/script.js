c0588def4acefdbfd1c2f7d961dbc364391630de
burillu <burillu90@hotmail.it>
2023-10-23 16:43:07 +0200

    add fn printcards

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const ourTeam = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        picture: 'wayne - barnett - founder - ceo.jpg'
+        picture: 'wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
@@ -32,15 +32,45 @@ const ourTeam = [
         picture: 'barbara-ramos-graphic-designer.jpg'
     },
 ]
-
+// prendo il divRow
+const divRow = document.getElementById('cardsRow');
 //console.log( ourTeam);
-for (let index = 0; index < ourTeam.length; index++) {
-    const element = ourTeam[index];
-    let name = element.name;
-    console.log(index+" "+ name);
-    let role = element.role;
-    console.log(index + " " + role);
-    let picture = element.picture;
-    console.log(index +" "+  picture)   
-    console.log('***********************'); 
+// stampo le cards
+printCards()
+
+
+
+
+/********************************************************************************************** */
+// FUNZIONI
+
+// funzione che stampa le cards a schermo
+function printCards() {
+
+    for (let index = 0; index < ourTeam.length; index++) {
+        const element = ourTeam[index];
+        const divCol = document.createElement('div');
+        divCol.classList.add('col');
+
+        let name = element.name;
+        //console.log(index + " " + name);
+        let role = element.role;
+        //console.log(index + " " + role);
+        let picture = element.picture;
+        console.log(index + " " + picture)
+        //console.log('***********************');
+        const template = `<div class="card my-card">
+<img src="img/${picture}" class="card-img-top" alt="${name}">
+<div class="card-body">
+  <h5 class="card-title">${name} </h5>
+  <p class="card-text">${role}</p>
+  
+</div>
+</div>`;
+divCol.innerHTML= template;
+divRow.append(divCol);
+    }
+    
+
 }
+
